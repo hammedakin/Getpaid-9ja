c7a9bf5c3695cff596b017c47cfa87c24abeb14d
omoluabii <hakra911@gmail.com>
2020-12-23 03:47:58 +0100

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/js/bootstrap'
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'boxicons'
 
